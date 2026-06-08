e206da2d1239d67058b527a7a50682106d3e5df3
Kiversayz <juzama12@gmail.com>
2026-06-08 22:05:17 +0300

    feat (db): Added Column email

--- app/models.py
+++ app/models.py
@@ -27,6 +27,7 @@ class Player(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), unique=True, index=True)
+    email = Column(String(100), unique=True, nullable=True)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     is_active = Column(Boolean, default=True)
 
